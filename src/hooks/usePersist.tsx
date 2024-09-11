6b841df65dfd79aa18d86e11696ffc0367bad53c
Anton Migulev <migulev@yandex.ru>
2024-09-10 20:51:56 -0400

    feat: usePersist add

---
+++ src/hooks/usePersist.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+interface PersistOptions<T> {
+  converter?: (value: string) => T
+}
+
+export function usePersist<T = string>(
+  key: string,
+  initialValue: T,
+  options?: PersistOptions<T>
+) {
+  const [value, setValue] = useState<T>(() => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue !== null) {
+      if (options?.converter) {
+        return options.converter(savedValue)
+      }
+      return (
+        typeof initialValue === 'string' ? savedValue : JSON.parse(savedValue)
+      ) as T
+    }
+    return initialValue
+  })
+
+  useEffect(() => {
+    localStorage.setItem(
+      key,
+      typeof value === 'string' ? value : JSON.stringify(value)
+    )
+  }, [key, value])
+
+  return [value, setValue] as const
+}
