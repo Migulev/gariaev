25e69a412068c4ad37df74015473646cf795cabb
Anton Migulev <migulev@yandex.ru>
2024-09-20 15:55:02 -0400

    refactor: use-delete-matrix

--- src/hooks/useDeleteMatrix.tsx
+++ src/hooks/useDeleteMatrix.tsx
@@ -9,7 +9,7 @@ export const useDeleteMatrix = () => {
   const deleteMatrix = async (matrix: Matrix) => {
     const confirmation = await getConfirmation({
       title: 'Удаление матрицы',
-      description: `Вы уверены, что хотите удалить матрицу ${matrix.title}?`,
+      description: `Вы уверены, что хотите удалить матрицу "${matrix.title}"?`,
     })
 
     if (confirmation) {
